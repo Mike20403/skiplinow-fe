9549e603ff20bf858b5d34c37d96941687e3d373
khuong.mai@nexpando <maikhuong2003@gmail.com>
2024-07-28 23:15:45 +0700

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { ToastContainer } from 'react-toastify';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './index.css';
+import { Route, Routes } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard.tsx';
-import { HomePage } from './pages/auth/HomePage';
 import { SMSAuthPage } from './pages/auth/SMSAuthPage';
 import { Toaster } from './components/ui/toaster';
 import ProtectedRoute from './components/route/ProtectedRoute';
@@ -13,9 +10,8 @@ import { CaptionProfilePage } from '@/pages/profile/CaptionProfilePage.tsx';
 import { CaptionGenerateHomePage } from '@/pages/services/CaptionGenerateHomePage.tsx';
 import { ChooseMediaPage } from '@/pages/services/start-from-scratch/ChooseMediaPage.tsx';
 import { GetInpsiredPage } from '@/pages/services/get-inspired/GetInspiredPage.tsx';
-import { MediaFormPage } from '@/pages/services/start-from-scratch/MediaFormPage.tsx';
 import SharePage from '@/pages/third-party/SharePage.tsx';
-
+import './index.css';
 
 function App() {
   return (
@@ -35,7 +31,6 @@ function App() {
             <Route path="get-inspired" element={<GetInpsiredPage />} />
           </Route>
 
-
           <Route path="profile" element={<CaptionProfilePage />} />
         </Route>
         <Route path="/share" element={<SharePage />} />
@@ -45,10 +40,17 @@ function App() {
             <PublicRoute>
               <SMSAuthPage />
             </PublicRoute>
-          }/>
-        <Route path="*" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-
       <Toaster />
       <ToastContainer closeOnClick />
     </>
